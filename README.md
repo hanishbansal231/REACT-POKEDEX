a9af098591d89b7e3286afa5d078f243992022d6
hanishbansal231 <hanishbansal803@gmail.com>
2023-10-03 23:41:41 +0530

    Set Readme File

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-# React + Vite
+# Image Gallery
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+### Setup instruction
 
-Currently, two official plugins are available:
+1. Clone the project
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+```
+   git clone https://github.com/hanishbansal231/REACT-POKEDEX.git
+```
+
+2. Move into the directory
+
+```
+   cd .\pokedex\    
+```
+
+3. install dependencies
+
+```
+   npm i 
+```
+
+4. Run the server
+
+```
+   npm run dev
+```
+
+### Adding plugins and dependencies
+
+```
+npm install  react-router-dom  axios 
+```
--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -2,66 +2,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import './PokemonList.css';
 import Pokemon from "../Pokemon/Pokemon";
+import usePokemonList from "../../hooks/usePokemonList";
 function PokemonList() {
-
-    const [pokemonListState, setPokemonListState] = useState({
-        pokemonList: [],
-        isLoading: false,
-        pokedexUrl: 'https://pokeapi.co/api/v2/pokemon',
-        nextUrl: '',
-        prevUrl: '',
-    });
-
-    async function downloadPokemons() {
-        setPokemonListState((prev) => ({ ...prev, isLoading: true }));
-
-        const response = await axios.get(pokemonListState.pokedexUrl); // this downloads list of 20 pokemons
-        const pokemonResult = response?.data?.results; // we get the array of pokemons from result
-        console.log(response?.data)
-
-        setPokemonListState((prev) => (
-            {
-                ...prev,
-                nextUrl: response?.data?.next,
-                prevUrl: response?.data?.previous
-            }
-        ));
-
-        // iterating over the array of pokemons, and using their url, to create an array of promises
-        // that will download those 20 pokemons
-        const pokemonResultPromis = pokemonResult.map((item) => axios.get(item?.url));
-
-
-        // passing the promise array to axios.all
-        const pokemonData = await axios.all(pokemonResultPromis);
-        // console.log(pokemonData);
-
-
-        // now iterate on the data of each pokemon, and extract id,name,image,types
-        const pokeListResult = pokemonData?.map((pokeData) => {
-            const pokemon = pokeData?.data;
-            return {
-                id: pokemon.id,
-                name: pokemon?.name,
-                image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny,
-                type: pokemon.types,
-            }
-        });
-
-        setPokemonListState((prev) => (
-            { 
-                ...prev, 
-                pokemonList: pokeListResult, 
-                isLoading: false 
-            }
-        ));
-    }
-
-
-    useEffect(() => {
-        downloadPokemons();
-    }, [pokemonListState.pokedexUrl]);
-
+    const {pokemonListState,setPokemonListState} = usePokemonList();
     return (
         <div className="pokemon-list-wrapper">
             <div className="pokemon-wrapper">
